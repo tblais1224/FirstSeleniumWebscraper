5ea4c30a8307126a465fac48cba84a02f1736170
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-01 13:09:13 -0400

    first simple web scraper finished

--- FirstSeleniumWebScraper/Program.cs
+++ FirstSeleniumWebScraper/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace FirstSeleniumWebScraper
 {
@@ -10,6 +12,24 @@ namespace FirstSeleniumWebScraper
     {
         static void Main(string[] args)
         {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://testing-ground.scraping.pro/login");
+
+                var userNameField = driver.FindElementById("usr");
+                var userPasswordField = driver.FindElementById("pwd");
+                var loginButton = driver.FindElementByXPath("//input[@value='Login']");
+
+                userNameField.SendKeys("admin");
+                userPasswordField.SendKeys("12345");
+                loginButton.Click();
+
+                var result = driver.FindElementByXPath("//div[@id='case_login']/h3").Text;
+
+                File.WriteAllText("result.txt", result);
+
+                driver.GetScreenshot().SaveAsFile(@"screenshottest.png", ScreenshotImageFormat.Png);
+            }
         }
     }
 }
